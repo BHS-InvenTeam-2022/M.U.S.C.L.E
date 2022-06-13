4961a58f271f1e1345161355a46265bd59da7bd1
kd993595 <kepedu17@gmail.com>
2022-06-13 00:15:26 -0400

    Update main.go

--- Restapi/main.go
+++ Restapi/main.go
@@ -326,6 +326,6 @@ func main() {
 
 	//myRouter.HandleFunc("/article/{id}", userInfo)
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", handlers.CORS(originsOk, headersOk, methodsOk)(myRouter)))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(myRouter)))
 	//dbinterface.ReadTable()
 }
